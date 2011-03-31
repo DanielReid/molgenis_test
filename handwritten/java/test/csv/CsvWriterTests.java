3c2fd90313fb40bbc3fb4e4e4c094d93ea1969b9
Danny Arends <danny.arends@gmail.com>
2011-03-31 11:48:13 +0000

    Updated the test, because of the Entity != Entity
    git-svn-id: http://www.molgenis.org/svn/molgenis_test/trunk@7320 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- handwritten/java/test/csv/CsvWriterTests.java
+++ handwritten/java/test/csv/CsvWriterTests.java
@@ -5,14 +5,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.molgenis.model.MolgenisModelException;
+import  org.molgenis.model.elements.Entity;
+
 import molgenis_test.fields.VarcharEntity;
 import app.CsvExport;
 
 public class CsvWriterTests
 {
-	public void testNullFields() throws IOException
+	public void testNullFields() throws IOException, MolgenisModelException
 	{
-		List<VarcharEntity> vlist = new ArrayList<VarcharEntity>();
+		List<? extends Entity> vlist = new ArrayList();
 		for(int i = 0; i < 3; i++)
 		{
 			VarcharEntity e = new VarcharEntity();
