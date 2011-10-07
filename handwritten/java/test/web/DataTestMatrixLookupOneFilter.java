9cfa80196acd87f833f9bc98a3084ab4b4d2b2f4
Harm Jan Westra <harm-jan@attix.nl>
2011-10-07 13:17:26 +0000

    Added multiple tests for the Molgenis Matrix viewer. Changed DataTestMatrix.java to base class.
    git-svn-id: http://www.molgenis.org/svn/molgenis_test/trunk@9312 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

---
+++ handwritten/java/test/web/DataTestMatrixLookupOneFilter.java
@@ -0,0 +1,45 @@
+package test.web;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DataTestMatrixLookupOneFilter {
+	static DataTestMatrix DataTestMatrix = new DataTestMatrix();
+
+	@Test
+	public void init() throws Exception {
+		// TODO: Make Selenium download te Molgenis Matrix export file
+		DataTestMatrix.file = "d:/ll_pheno_quantitatief_one_filter_subset.txt";
+		DataTestMatrix.getMatrixColumnsIndex();
+		if (DataTestMatrix.getDbTablesColumns() == false)
+			Assert.assertFalse(true);
+	}
+
+	@Test(dependsOnMethods = { "init" })
+	public void testMakeGlobalTable() throws Exception {
+		DataTestMatrix.makeGlobalTable();
+	}
+
+	@Test(dependsOnMethods = { "testMakeGlobalTable" })
+	public void testFillGlobalTable() throws Exception {
+		DataTestMatrix.fillGlobalTable();
+	}
+
+	@Test(dependsOnMethods = { "testFillGlobalTable" })
+	public void testMakeTables() throws Exception {
+		DataTestMatrix.makeTables();
+	}
+
+	@Test(dependsOnMethods = { "testMakeTables" })
+	public void testFillTables() throws Exception {
+		DataTestMatrix.fillTables();
+	}
+
+	@Test(dependsOnMethods = { "testFillTables" })
+	public void testCompareTablesOneFilter() throws Exception {
+		if (DataTestMatrix.compareFilteredTables("BEZOEK1", "GEWICHT", "=",
+				"50", null, null, null, null) == false)
+			Assert.assertFalse(true);
+	}
+
+}
