f22fc9ee4419d27dab33ca332312739d93ab43d4
Harm-Jan Westra <westra.harmjan@gmail.com>
2011-10-26 14:56:35 +0000

    - use of 'case when' to support filters on the EAV model - Complete, one filter and two filters are tested and o.k.
    git-svn-id: http://www.molgenis.org/svn/molgenis_test/trunk@9446 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- handwritten/java/test/web/DataTestMatrixEAVLookupTwoFilters.java
+++ handwritten/java/test/web/DataTestMatrixEAVLookupTwoFilters.java
@@ -8,31 +8,54 @@ public class DataTestMatrixEAVLookupTwoFilters {
 
 	@Test
 	public void init() throws Exception {
-		// TODO: Make Selenium download te Molgenis Matrix export file.
+
 		DataTestMatrixEAV.file = "/pheno_two_filters.txt";
 		DataTestMatrixEAV.investigation = "50";
-		DataTestMatrixEAV.filter1Table = "BEZOEK1";
-		DataTestMatrixEAV.filter1Column = "GEWICHT";
-		DataTestMatrixEAV.filter1Operator = ">";
-		DataTestMatrixEAV.filter1Value = "100";
-		DataTestMatrixEAV.filter2Table = "BEZOEK1";
-		DataTestMatrixEAV.filter2Column = "LENGTE";
-		DataTestMatrixEAV.filter2Operator = "<";
-		DataTestMatrixEAV.filter2Value = "170";
 
-		DataTestMatrixEAV.filter1 = true;
-		DataTestMatrixEAV.filter2 = true;
+		DataTestMatrixEAV.testTablePrefix = "T2_";
+		DataTestMatrixEAV.sourceTablePrefix = "LL_";
+		DataTestMatrixEAV.matrixSeperator = "\t";
+		DataTestMatrixEAV.testOwner = "MOLGENIS";
+
+		DataTestMatrixEAV.dbDriver = "oracle.jdbc.driver.OracleDriver";
+		DataTestMatrixEAV.dbUrl = "jdbc:oracle:thin:@//localhost:2000/llptest";
+		DataTestMatrixEAV.dbUsername = "molgenis";
+		DataTestMatrixEAV.dbPassword = "molTagtGen24Ora";
+
+		// inverse filters
+		String filter1Table = "BEZOEK1";
+		String filter1Column = "MEETSTAND";
+		String filter1Operator = "<>";
+		String filter1Value = "1";
+		String filter2Table = "BEZOEK1";
+		String filter2Column = "GEWICHT";
+		String filter2Operator = "<>";
+		String filter2Value = "80";
+
+		DataTestMatrixEAV.caseWhenEndAs = "end as";
+		DataTestMatrixEAV.caseWhenCondition = "case when (p.name = '"
+				+ DataTestMatrixEAV.sourceTablePrefix + filter1Table
+				+ "' and oe.name = '" + filter1Column + "' and ov.value "
+				+ filter1Operator + " '" + filter1Value + "') or (p.name = '"
+				+ DataTestMatrixEAV.sourceTablePrefix + filter2Table
+				+ "' and oe.name = '" + filter2Column + "' and ov.value "
+				+ filter2Operator + " '" + filter2Value + "') then null else ";
+
+		DataTestMatrixEAV.init();
+
 		if (DataTestMatrixEAV.parseMatrixColumns() == false)
 			Assert.assertFalse(true);
 	}
 
 	@Test(dependsOnMethods = { "init" })
-	public void testMakeGlobalTable() throws Exception {
-		DataTestMatrixEAV.makeGlobalTable();
+	public void testparseMatrixColumns() throws Exception {
+		if (DataTestMatrixEAV.parseMatrixColumns() == false)
+			Assert.assertFalse(true);
 	}
 
-	@Test(dependsOnMethods = { "testMakeGlobalTable" })
+	@Test(dependsOnMethods = { "testparseMatrixColumns" })
 	public void testFillGlobalTable() throws Exception {
+		DataTestMatrixEAV.makeGlobalTable();
 		DataTestMatrixEAV.fillGlobalTable();
 	}
 
@@ -41,5 +64,4 @@ public class DataTestMatrixEAVLookupTwoFilters {
 		if (DataTestMatrixEAV.compareGlobalTableToEAV() == false)
 			Assert.assertFalse(true);
 	}
-
 }
