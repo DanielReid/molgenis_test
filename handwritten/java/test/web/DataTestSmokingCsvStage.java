2170a786d762417b5969b4987e33a30ff0b11019
Harm Jan Westra <harm-jan@attix.nl>
2012-02-02 12:14:31 +0000

    updated smoking validation
    git-svn-id: http://www.molgenis.org/svn/molgenis_test/trunk@10699 42de9c89-b41c-0410-9fa9-f33e7b6bda7a

--- handwritten/java/test/web/DataTestSmokingCsvStage.java
+++ handwritten/java/test/web/DataTestSmokingCsvStage.java
@@ -8,7 +8,9 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import au.com.bytecode.opencsv.CSVReader;
@@ -16,7 +18,7 @@ import au.com.bytecode.opencsv.CSVReader;
 public class DataTestSmokingCsvStage {
 
 	// Input CSV file
-	String csvFile = "d:/smoking.csv";
+	String csvFile = "H:/Data/Smoking/smoking.csv";
 
 	// MSSQL parameters
 	String serverMSSQL = "WTZKH0077";
@@ -25,26 +27,28 @@ public class DataTestSmokingCsvStage {
 	String dbUrlMSSQL = "jdbc:sqlserver://" + serverMSSQL + ";databaseName="
 			+ databaseMSSQL + ";integratedSecurity=true";
 
-	// Exclusion parameter
-	String[] excludedColumns = new String[] { "LL_NR", "WGA_LLNR", "GESLACHT",
-			"GEBDAT", "gwa" };
-
-	ArrayList<String> columns = new ArrayList<String>();
 	Connection connMSSQL;
 	Statement stmtMSSQL;
 	ResultSet rsetMSSQL;
 
+	CSVReader reader;
+	String sql;
+	List<String> columnsIndex = new ArrayList<String>();
+	List<String[]> csvData = new ArrayList<String[]>();
+	List<String[]> dbData = new ArrayList<String[]>();
+
 	@Test
 	public void testInit() throws Exception {
-		System.out.println("WARNING Exluded CSV columns: "
-				+ Arrays.asList(excludedColumns));
 		connectDatabase();
 		getColumnsIndex();
+		getCsvData();
+		getDbData();
+		compare();
 	}
 
 	@Test(dependsOnMethods = { "testInit" })
 	public void testCompare() throws Exception {
-		if (lookupDataRows()) {
+		if (false) {
 			Assert.assertFalse(true);
 		}
 	}
@@ -65,91 +69,163 @@ public class DataTestSmokingCsvStage {
 
 	public void getColumnsIndex() throws Exception {
 		System.out.println("Getting column index...");
-		CSVReader reader = new CSVReader(new InputStreamReader(
-				new FileInputStream(csvFile), "UTF-8"));
-		String[] headerLine = reader.readNext();
-		for (int i = 0; i < headerLine.length; i++) {
-			columns.add(headerLine[i]);
+		reader = new CSVReader(new InputStreamReader(new FileInputStream(
+				csvFile), "UTF-8"));
+		String[] columns = reader.readNext();
+		for (String column : columns)
+			columnsIndex.add(column);
+	}
+
+	public void getCsvData() throws Exception {
+		csvData = reader.readAll();
+		reader.close();
+	}
+
+	public void getDbData() throws Exception {
+		sql = "select ";
+		for (String column : columnsIndex)
+			sql += column + ", ";
+		sql = sql.substring(0, sql.length() - 2) + " from smoking";
+		rsetMSSQL = stmtMSSQL.executeQuery(sql);
+		while (rsetMSSQL.next()) {
+			List<String> lRow = new ArrayList<String>();
+			for (int i = 1; i <= columnsIndex.size(); i++) {
+				if (rsetMSSQL.getString(i) == null)
+					lRow.add("");
+				else
+					lRow.add(rsetMSSQL.getString(i));
+			}
+			String[] saRow = lRow.toArray(new String[lRow.size()]);
+			dbData.add(saRow);
 		}
 	}
 
-	public boolean lookupDataRows() throws Exception {
+	public void compare() throws Exception {
 		boolean fail = false;
-		System.out.println("Lookup data rows...");
-
-		CSVReader reader = new CSVReader(new InputStreamReader(
-				new FileInputStream(csvFile), "UTF-8"));
-		String[] line;
-		// Skip header.
-		reader.readNext();
-		Integer totalCounter = 1;
-		Integer counter = 1;
-		while ((line = reader.readNext()) != null) {
+		int csvRowCount = csvData.size();
+		int dbRowCount = dbData.size();
+		if (csvRowCount != dbRowCount) {
+			fail = true;
+			System.out.println("FAIL: rowcount CSV file: " + csvRowCount
+					+ " database: " + dbRowCount);
+			System.out.println();
+		} else {
+			System.out.println("Rowcount: " + csvRowCount);
+			System.out.println();
+		}
+		// Lookup CSV in Database
+		int totalCounter = 0;
+		int stepCounter = 0;
+		System.out.println("Lookup CSV in Database [" + totalCounter + "/"
+				+ csvRowCount + "].");
+		for (String[] csvLine : csvData) {
 			totalCounter++;
-			counter++;
-			if (counter >= 500) {
-				counter = 0;
-				System.out.println("Rowcount: " + totalCounter);
+			stepCounter++;
+			if (stepCounter >= 500) {
+				stepCounter = 0;
+				System.out.println("Lookup CSV in Database [" + totalCounter
+						+ "/" + csvRowCount + "].");
+				System.out.println();
 			}
-			Boolean exception = false;
-			String sql = "select count(*) from smoking where ";
-			String diagnoseDbSelect = "select 'DATABASE RESULTS:' as SOURCE, ";
-			String diagnoseCsvSelect = "select 'CSV RESULTS:' as SOURCE, ";
-			String diagnoseId = "";
-			for (int i = 0; i < columns.size(); i++) {
-				try {
-					if (!Arrays.asList(excludedColumns)
-							.contains(columns.get(i))) {
-						// Handle null values
-						if (line[i].equals("")
-								|| line[i].toUpperCase().equals("NULL")) {
-							sql += columns.get(i) + " is null and ";
-							diagnoseCsvSelect += "null, ";
-						}
-						// Handle numeric values
-						else if (line[i].matches("[-+]?\\d+(\\.\\d+)?")) {
-							sql += columns.get(i) + " = " + line[i] + " and ";
-							diagnoseCsvSelect += line[i] + ", ";
-						}
-						// Handle string values
-						else {
-							sql += columns.get(i) + " = '"
-									+ line[i].replace("'", "''") + "' and ";
-							diagnoseCsvSelect += "'"
-									+ line[i].replace("'", "''") + "', ";
-						}
-
-						diagnoseDbSelect += columns.get(i) + ", ";
-						if (columns.get(i).toUpperCase().equals("ID"))
-							diagnoseId += line[i];
-					}
-				} catch (Exception e) {
-					exception = true;
+			int intAmountInDb = amountInDb(csvLine);
+			if (intAmountInDb > 1) {
+				// Look back
+				int intAmountInCsv = amountInCsv(csvLine);
+				if (intAmountInDb != intAmountInCsv) {
 					fail = true;
+					System.out.println("FAIL: Count db/csv: " + intAmountInDb
+							+ "/" + intAmountInCsv + ".");
+					System.out.println("CSV Line: " + getLine(csvLine));
+					System.out.println("SQL: " + getSqlLine(csvLine));
+					System.out.println();
 				}
 			}
-
-			diagnoseDbSelect = diagnoseDbSelect.substring(0,
-					diagnoseDbSelect.length() - 2)
-					+ " from smoking where id = " + diagnoseId;
-			diagnoseCsvSelect = diagnoseCsvSelect.substring(0,
-					diagnoseCsvSelect.length() - 2);
-
-			if (exception)
-				System.out.println("FAIL: Wrong line length. Line:"
-						+ Arrays.asList(line));
-			else {
-				sql = sql.substring(0, sql.length() - 4);
-				rsetMSSQL = stmtMSSQL.executeQuery(sql);
-				rsetMSSQL.next();
-				if (rsetMSSQL.getString(1).equals("0")) {
-					fail = true;
-					System.out.println("/* FAILED SQL: */ " + sql);
-					System.out.println("\t/* Diagnose SQL: */ "
-							+ diagnoseDbSelect + " union " + diagnoseCsvSelect);
-				}
+			if (intAmountInDb == 0) {
+				fail = true;
+				System.out.println("FAIL: Not found in database.");
+				System.out.println("CSV Line: " + getLine(csvLine));
+				System.out.println("SQL: " + getSqlLine(csvLine));
+				System.out.println();
 			}
 		}
-		return fail;
+		// Lookup Database in CVS
+		totalCounter = 0;
+		stepCounter = 0;
+		System.out.println("Lookup Database in CVS [" + totalCounter + "/"
+				+ dbRowCount + "].");
+		for (String[] dbLine : dbData) {
+			totalCounter++;
+			stepCounter++;
+			if (stepCounter >= 500) {
+				stepCounter = 0;
+				System.out.println("Lookup Database in CVS [" + totalCounter
+						+ "/" + dbRowCount + "].");
+				System.out.println();
+			}
+			int intAmountInCsv = amountInCsv(dbLine);
+			if (intAmountInCsv > 1) {
+				// Look back
+				int intAmountInDb = amountInDb(dbLine);
+				if (intAmountInCsv != intAmountInDb) {
+					fail = true;					
+					System.out.println("FAIL: Count db/csv: " + intAmountInDb
+							+ "/" + intAmountInCsv + ".");
+					System.out.println("CSV Line: " + getLine(dbLine));
+					System.out.println("SQL: " + getSqlLine(dbLine));
+					System.out.println();				
+				}
+			}
+			if (intAmountInCsv == 0) {
+				fail = true;
+				System.out.println("FAIL: Not found in CSV.");
+				System.out.println("CSV Line: " + getLine(dbLine));
+				System.out.println("SQL: " + getSqlLine(dbLine));
+				System.out.println();
+			}
+		}
+
 	}
+
+	public int amountInDb(String[] line) {
+		int maches = 0;
+		for (String[] dbLine : dbData) {
+			if (Arrays.equals(line, dbLine))
+				maches++;
+		}
+		return maches;
+	}
+
+	public int amountInCsv(String[] line) {
+		int maches = 0;
+		for (String[] csvLine : csvData) {
+			if (Arrays.equals(line, csvLine))
+				maches++;
+		}
+		return maches;
+	}
+
+	public String getLine(String[] line) {
+		String strLine = "";
+		for (int i = 0; i < line.length; i++)
+			strLine += line[i] + ",";
+		return strLine.substring(0, strLine.length() - 1);
+	}
+
+	public String getSqlLine(String[] line) {
+		String sql1 = sql + " where ";
+		for (int i = 0; i < line.length; i++) {
+			// Handle null values
+			if (line[i].equals("") || line[i].toLowerCase().equals("null"))
+				sql1 += columnsIndex.get(i) + " is null and ";
+			// Handle numeric values
+			else if (line[i].matches("[-+]?\\d+(\\.\\d+)?"))
+				sql1 += columnsIndex.get(i) + " = " + line[i] + " and ";
+			// Handle string values
+			else
+				sql1 += columnsIndex.get(i) + " = '"
+						+ line[i].replace("'", "''") + "' and ";
+		}
+		return sql1.substring(0, sql1.length() - 4);
+	}
+
 }
